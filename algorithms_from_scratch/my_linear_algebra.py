7a2ef08f862f975d9fc6f00c60ff31efb3e8dd04
Mgo <mgotm13@gmail.com>
2021-08-12 00:36:29 +0400

    specific linear algebra code modifications

--- algorithms_from_scratch/my_linear_algebra.py
+++ algorithms_from_scratch/my_linear_algebra.py
@@ -2,9 +2,15 @@ class Vector:
     def __init__(self):
         pass
 
+    def __add__(self, other):
+        pass
 
-class Matrix:
+
+class Matrix(Vector):
     def __init__(self):
+        pass
+
+    def __add__(self, other):
         pass
 
 # TODO maybe try Tensor :) or maybe as a task
