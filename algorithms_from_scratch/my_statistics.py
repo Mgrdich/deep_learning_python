c52448cd7f828fb1263167ccc0b5fe3b7e799b58
Mgo <mgotm13@gmail.com>
2021-09-02 00:40:34 +0400

    adding statistics module

--- algorithms_from_scratch/my_statistics.py
+++ algorithms_from_scratch/my_statistics.py
@@ -1,4 +1,48 @@
+from __future__ import division
+from collections import Counter
+
+from my_linear_algebra import Vector
+from util.typings import NUMBER
+
+
+# TODO maybe make an inheritance with the Vector method
 
 class Statistics:
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def mean(x: Vector) -> NUMBER:
+        return sum(x) / len(x)
+
+    @staticmethod
+    def median(x: Vector) -> NUMBER:
+        n = len(x)
+        sorted_arr = sorted(x)
+        mid_point = n // 2
+
+        if n % 2 == 1:
+            return sorted_arr[mid_point]
+
+        lo = mid_point - 1
+        hi = mid_point
+        return (sorted_arr[lo] + sorted_arr[hi]) / 2
+
+    @staticmethod
+    def quantile(x: Vector, p: NUMBER) -> NUMBER:
+        """
+        :param x: a Vector of Numbers
+        :param p: the percentile value
+        :return pth-percentile value in x
+        """
+        p_index = int(p * len(x))
+        return sorted(x)[p_index]
+
+    @staticmethod
+    def mode(x: Vector):
+        counts = Counter(x)
+        max_count = max(counts.values())
+        return [x_i for x_i, count in counts.items()
+                if count == max_count]
+
+    @staticmethod
+    def data_range(x: Vector):
+        return max(x) - min(x)
