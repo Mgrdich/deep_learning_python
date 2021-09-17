0091797178a8149d6bc414adf6fd470646a64e09
Mgo <mgotm13@gmail.com>
2021-09-18 00:00:17 +0400

    minor changes in the decorator wrapper function

--- util/decorators.py
+++ util/decorators.py
@@ -6,7 +6,9 @@ exception_decorators = dict()
 
 
 def register_exception_decorators(function: Callable):
-    # TODO add plugin unique name error
+    if function.__name__ in exception_decorators:
+        raise Exception('Function with this name has been registered')
+
     exception_decorators[function.__name__] = function
     return function
 
@@ -14,6 +16,7 @@ def register_exception_decorators(function: Callable):
 @register_exception_decorators
 def is_number(is_method: bool):
     def decorator(function):
+        @wraps(function)
         def wrapper(*args, **kwargs):
             if is_method:
                 element = args[1]
